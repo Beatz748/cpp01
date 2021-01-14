a1fd8f453348ec0ba33819abc7bf454d06f18c22
UGF3ZWwgTW9za2Fsw7xr TW9za2Fsw7xr <kshantel@ga-u1.msk.21-school.ru>
2021-01-15 00:08:27 +0300

    atart

---
+++ ex00/classPony.hpp
@@ -0,0 +1,23 @@
+#ifndef CLASSPONY_HPP
+# define CLASSPONY_HPP
+
+# include <string>
+
+
+using	std::string;
+
+class Pony
+{
+	private:
+		string	name;
+		int		age;
+		string	sex;
+		
+	public:
+		Pony();
+		~Pony();
+		void
+};
+
+
+#endif
